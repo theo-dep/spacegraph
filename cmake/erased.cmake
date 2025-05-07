81e91e4a06fc613b1c2b4eaddce115e9b387c647
Théo Devaucoup <theo.devaucoup@pm.me>
2025-05-07 23:11:53 +0200

    move Erased as submodule

--- cmake/erased.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(erased
-    GIT_REPOSITORY https://github.com/qnope/Erased.git
-    GIT_TAG 544529135a0daa0152c1957932b610d1ccfa6ba9
-    EXCLUDE_FROM_ALL
-)
-
-FetchContent_MakeAvailable(erased)
