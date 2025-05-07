3b646eed9ef5167abf4fb818f03dcf7a064865c4
Théo Devaucoup <theo.devaucoup@pm.me>
2025-05-07 22:56:51 +0200

    use fixed erased commit hash

--- cmake/erased.cmake
+++ cmake/erased.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(erased
     GIT_REPOSITORY https://github.com/qnope/Erased.git
-    GIT_TAG main
+    GIT_TAG 544529135a0daa0152c1957932b610d1ccfa6ba9
     EXCLUDE_FROM_ALL
 )
 
