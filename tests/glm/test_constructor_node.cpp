2f47557b0b8200d58d1797fca9045b7835d2a5c2
Théo Devaucoup <theo.devaucoup@pm.me>
2025-05-12 21:50:13 +0200

    fix variable unused compilation error

--- tests/glm/test_constructor_node.cpp
+++ tests/glm/test_constructor_node.cpp
@@ -4,10 +4,10 @@
 
 TEST_CASE("Construct a node from in place constructor", "[constructor_node]")
 {
-    glmNode node(std::in_place, 1.f);
+    [[maybe_unused]] glmNode _(std::in_place, 1.f);
 }
 
 TEST_CASE("Construct a node from copy constructor", "[constructor_node]")
 {
-    glmNode node(glm::mat4(1));
+    [[maybe_unused]] glmNode _(glm::mat4(1));
 }
